e7f3100a6bd15a89631718aa993d41985fb7c1f9
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-02-13 21:00:28 +0000

    init

---
+++ application.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var corser = require("corser");
+
+var app = express();
+app.use(corser.create());
+
+app.use(express.static(__dirname + '/dist'));
+
+app.options("*", function (req, res) {
+  // CORS
+  res.writeHead(204);
+  res.end();
+});
+
+var port = process.env.GKE_NODEJS_PORT || 8080;
+var host = process.env.GKE_NODEJS_IP || '0.0.0.0';
+var server = app.listen(port, host, function() {
+  console.log("App started at: " + new Date() + " on port: " + port);
+});
+module.exports = server;
